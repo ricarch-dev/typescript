bfdbc36ad2c5e932beb9176d9af50fa03dca52ce
Ricardo Chavez <chavezmataricardo@gmail.com>
2025-04-08 14:16:25 -0400

    feat: implement boolean utility functions with logging

---
+++ src/05-boolean.ts
@@ -0,0 +1,13 @@
+(() => {
+  let isEnable = true;
+  isEnable = false;
+  //explicity type
+  let isNew: boolean = false;
+  console.log(isNew);
+  isNew = true;
+  console.log(isNew);
+  const randomNumber = Math.random();
+  console.log(randomNumber);
+  isNew = randomNumber >= 0.5 ? true : false;
+  console.log(isNew);
+})();
